b676b1000a3b72c244945c9a43feee10cebd9a4b
ethanlenox <noxleviox@gmail.com>
2026-09-14 05:29:18 +0200

    Update music-player.js

--- js/music-player.js
+++ js/music-player.js
@@ -75,13 +75,13 @@ const musicPlaylist = [
 
  
     {
-        title: "Asteroid Destroyer Theme",
-        src: "son/asteroid destroyer theme musicale.mp3"
+        title: "Comet",
+        src: "son/Comet.mp3"
     },
 
     {
-        title: "Alerte astre",
-        src: "son/alerte astre.mp3"
+        title: "cosmic sylver",
+        src: "son/cosmic sylver.mp3"
     },
  
     {
@@ -95,33 +95,52 @@ const musicPlaylist = [
     },
 
     {
-        title: "Tension",
-        src: "son/tension.mp3"
+        title: "flash ecarlate",
+        src: "son/flash ecarlate.mp3"
     },
 
     {
-        title: "Confusion",
-        src: "son/confusion.mp3"
+        title: "metal chaos",
+        src: "son/metal chaos.mp3"
     },
 
     
     {
-        title: "Fight",
-        src: "son/fight.mp3"
+        title: "Pixel Rush",
+        src: "son/Pixel Rush.mp3"
     },
  
 
     {
-        title: "Reflection",
-        src: "son/reflection.mp3"
+        title: "retro gold",
+        src: "son/retro gold.mp3"
     },
 
     {
-        title: "Revolt",
-        src: "son/revolt.mp3"
+        title: "rupture celeste",
+        src: "son/rupture celeste.mp3"
     },
 
-    
+    {
+        title: "stone fire",
+        src: "son/stone fire.mp3"
+    },
+
+    {
+        title: "temporis",
+        src: "son/temporis.mp3"
+    },
+
+    {
+        title: "stylise rock",
+        src: "son/stylise rock.mp3"
+    },
+
+    {
+        title: "union world",
+        src: "son/union world.mp3"
+    },
+ 
     {
         title: "Boss",
         src: "son/boss.mp3"
